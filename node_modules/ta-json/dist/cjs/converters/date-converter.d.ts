60641e03eb08b86d7e8c3d233297f180c472c156
hoangnam <namnguyen.fdev@gmail.com>
2020-11-30 11:05:31 +0700

    remove node

--- node_modules/ta-json/dist/cjs/converters/date-converter.d.ts
+++
@@ -1,5 +0,0 @@
-import { IPropertyConverter } from "./converter";
-export declare class DateConverter implements IPropertyConverter {
-    serialize(property: Date): string;
-    deserialize(value: string): Date;
-}
