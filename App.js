97c1cc3aafd9b1e36e72784923a4685f4e62d25a
namnguyen21-f <namnguyen.fdev@gmail.com>
2021-01-12 10:07:06 +0700

    c

--- App.js
+++ App.js
@@ -13,7 +13,7 @@ const __dirname = dirname(__filename);
 var url = "https://gruop08app.herokuapp.com";
 const app = express();
 
-const stringEB = "https://gruop08app.herokuapp.com";
+const stringEB = "http://localhost:3000";
 //https://gruop08app.herokuapp.com
 
 mongoose.Promise = global.Promise;
